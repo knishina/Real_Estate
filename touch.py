a54ca5ea9217c274f46b2978b9881d2a8a63806c
spease99 <scott.pease@comcast.net>
2018-03-24 10:57:06 -0700

    testing 2

---
+++ touch.py
@@ -0,0 +1,12 @@
+ Volume in drive C is OS
+ Volume Serial Number is AEC3-1448
+
+ Directory of C:\Users\Scott\Real_Estate
+
+03/24/2018  10:49 AM    <DIR>          .
+03/24/2018  10:49 AM    <DIR>          ..
+03/24/2018  10:37 AM                33 clean_sp.py
+03/24/2018  10:28 AM                37 README.md
+03/24/2018  10:49 AM                 0 touch.py
+               3 File(s)             70 bytes
+               2 Dir(s)  312,152,129,536 bytes free
