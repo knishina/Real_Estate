45c9b73830d5ee70889cad90f77adad0f485cf72
Suresh Adda <asureshb@gmail.com>
2018-03-29 19:32:58 -0700

    Scripts to Scrape & Process web data & South Carolina Crime Data

--- CrimeData/structure_crime_report.py
+++ CrimeData/structure_crime_report.py
@@ -3,6 +3,17 @@
 # this application is to conver unstructured raw crime data into csv structured format
 # import dependencies here
 
+
+"""
+Format of unstructured crime report:
+
+Zip29003:Crime rates in Bamberg, SC:Total185Violent Crime19Murder & Manslaughter0Forcible Rape1Robbery3Aggravated Assault15Property Crime166Burglary37Larceny & Theft123Motor Vehicle Theft6Arson5
+Zip29006:Crime rates in Batesburg-Leesville, SC:Total390Violent Crime67Murder & Manslaughter0Forcible Rape3Robbery3Aggravated Assault61Property Crime323Burglary55Larceny & Theft239Motor Vehicle Theft29Arson1
+Zip29009:Crime rates in Bethune, SC:Total5Violent Crime0Murder & Manslaughter0Forcible Rape0Robbery0Aggravated Assault0Property Crime5Burglary2Larceny & Theft2Motor Vehicle Theft1Arson0
+Zip29010:Crime rates in Bishopville, SC:Total311Violent Crime23Murder & Manslaughter0Forcible Rape0Robbery5Aggravated Assault18Property Crime288Burglary60Larceny & Theft213Motor Vehicle Theft15Arson1
+Zip29018:Crime rates in Bowman, SC:Total35Violent Crime10Murder & Manslaughter1Forcible Rape0Robbery0Aggravated Assault9Property Crime25Burglary11Larceny & Theft11Motor Vehicle Theft3Arson0
+"""
+
 import re
 from pprint import pprint
 import pandas as pd
